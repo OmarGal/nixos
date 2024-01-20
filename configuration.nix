9a24f8a44dbbc5cf002d812e882333714849346c
omar <omargalindol.01@gmail.com>
2024-01-19 19:57:59 -0600

    first commit

---
+++ configuration.nix
@@ -0,0 +1,187 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # NVIDIA
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Also sets NVreg_PreserveVideoMemoryAllocations=1, this fixed hibernate issues.
+    powerManagement.enable = true;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    enableNvidiaPatches = true;
+    xwayland.enable = true;
+  };
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    #  Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # Power management
+  services.logind.extraConfig = ''
+    HandlePowerKey=suspend
+    IdleAction=suspend
+    IdleActionSec=1m
+  '';
+
+  # Unfree software
+  nixpkgs.config.allowUnfree = true;
+
+  # Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Resume
+  security.protectKernelImage = false;
+  boot.resumeDevice = "/dev/nvme0n1p4";
+  boot.kernelParams = [ "resume_offset=16189440" ];
+
+  # idk
+#  systemd.services.systemd-logind.environment = {
+#   SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK = "1";
+#  };
+
+  # Swap file
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 10*1024;
+  }];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Static ip address
+  networking.interfaces.enp9s0.wakeOnLan.enable = true;
+  networking.interfaces.enp9s0.ipv4.addresses = [ {
+    address = "192.168.1.10";
+    prefixLength = 24;
+  } ];
+  #networking.useDHCP = lib.mkForce false;
+  networking.interfaces.enp9s0.useDHCP = false;
+  networking.defaultGateway = "192.168.1.254";
+  networking.nameservers = [ "1.1.1.1" ];
+
+  # Set your time zone.
+  time.timeZone = "America/Mexico_City";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Sound
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kiwi = {
+    isNormalUser = true;
+    description = "kiwi";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "kiwi";
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    font-awesome
+    (nerdfonts.override { fonts = [
+      "RobotoMono"
+    ];})
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim    
+    kitty
+    firefox
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
---
+++ swayidle.nix
@@ -0,0 +1,15 @@
+{ pkgs, config, lib, ...  }: {
+
+  services.swayidle = {
+    enable = true;
+    #systemdTarget = "hyprland-session.target";
+    systemdTarget = "xdg-desktop-portal-hyprland.service";
+    events = [
+    ];
+
+    timeouts = [{
+      timeout = 600; 
+      command = "${pkgs.systemd}/bin/systemctl hibernate";
+    }];
+  };
+}
